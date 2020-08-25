bdea9308e7e62b0f970d64684f9ed0cd09f51c9b
Jason Baker <jbaker@freightwaves.com>
2020-08-24 21:30:13 -0500

    Add boilerplate tests for worker

--- {{cookiecutter.project_slug}}/prediction/tests/test_prediction.py
+++ {{cookiecutter.project_slug}}/prediction/tests/test_prediction.py
@@ -1,23 +1,21 @@
 # from prediction.predictions import WorkerSettings
+import pytest
 from arq import create_pool
 import joblib
 from arq.connections import RedisSettings
-from ..worker import predict
+from worker.worker import predict
 import pandas as pd
 
-
-def test_model():
-
-    model = joblib.load('../model.joblib')
+@pytest.mark.asyncio
+async def test_model():
     df = pd.DataFrame({
         "a": [1, 2, 3, 4, 5],
         "b": [40, 50, 80, 200, 100],
         "c": [20, 100, 5, 40, 80]
     })
 
-
-    predictions = model.predict(df).prediction
+    predictions = await predict(None, 'testmodel', X=df)
 
 
     assert len(df) == len(predictions), "Predictions were different length than model."
-    assert predictions.min() > 0, "Minimum prediction less than zero"
+    assert predictions['prediction'].min() > 0, "Minimum prediction less than zero"
